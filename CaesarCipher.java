6a6bfbc9a631dde8355fce9ccd838bc6ce6d2b5f
Dmitry Savin <the.great.demetrius@gmail.com>
2021-10-08 13:58:22 +0400

    Create CaesarCipher.java

---
+++ CaesarCipher.java
@@ -0,0 +1,7 @@
+public class CaesarCipher {
+    public static void main(String args[]) {
+        String str = "Caesar cipher";
+        for(int i = 0; i < str.length(); i++)
+            System.out.print((char) (str.charAt(i) + 3));
+    }
+}
