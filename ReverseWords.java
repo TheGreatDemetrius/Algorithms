f381a246d09c3b66810c429ab43af83e5481c6d2
Dmitry Savin <the.great.demetrius@gmail.com>
2022-01-02 19:50:48 +0400

    Delete ReverseWords.java

--- ReverseWords.java
+++
@@ -1,7 +0,0 @@
-public class ReverseWords {
-    public static void main(String args[]) {
-        String words[] = "Reverse string".split("\\s");
-        for (int i = words.length - 1; i >= 0 ; i--)
-            System.out.print(words[i] + " ");
-    }
-}
