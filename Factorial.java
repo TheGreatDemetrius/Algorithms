1775be2ce32c35d24834df79e31251ad1828ed3f
Dmitry Savin <the.great.demetrius@gmail.com>
2022-01-02 18:59:53 +0400

    Convert to Java style

--- Factorial.java
+++ Factorial.java
@@ -1,9 +1,9 @@
 public class Factorial {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.print(fac(8));
     }
+
     static int fac(int n) {
-        if(n == 0) return 1;
-        return n * fac(n - 1);
+        return n == 0 ? 1 : n * fac(n - 1);
     }
 }
