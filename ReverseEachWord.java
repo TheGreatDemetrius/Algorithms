e355626e20c3263b88f79e80c4da156cf77c03d0
Dmitry Savin <the.great.demetrius@gmail.com>
2022-01-02 19:44:38 +0400

    Delete ReverseEachWord.java

--- ReverseEachWord.java
+++
@@ -1,7 +0,0 @@
-public class ReverseEachWord {
-    public static void main(String args[]) {
-         String words[] = "Reverse string".split("\\s");
-         for(String word : words)
-            System.out.print(new StringBuilder(word).reverse() + " ");
-     }
-}
