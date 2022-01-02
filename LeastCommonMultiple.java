93e05fb8ea7a24028d33618c7a6454dd3d0ba692
Dmitry Savin <the.great.demetrius@gmail.com>
2022-01-02 19:09:00 +0400

    Convert to Java style

--- LeastCommonMultiple.java
+++ LeastCommonMultiple.java
@@ -1,12 +1,13 @@
 public class LeastCommonMultiple{
-	public static void main(String[] args) {
-		System.out.print(lcm(2, 10));
-	}
-	static int gcd(int a, int b) {
-	    if (a == 0) return b;
-	    return gcd(b % a, a);
-	}
-	static int lcm(int a, int b) {
-	    return (a / gcd(a, b)) * b;
-	}
+    public static void main(String[] args) {
+        System.out.print(lcm(2, 8));
+    }
+
+    static int gcd(int a, int b) {
+        return a == 0 ? b : gcd(b % a, a);
+    }
+
+    static int lcm(int a, int b) {
+        return (a / gcd(a, b)) * b;
+    }
 }
