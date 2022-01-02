21c9b39d1059a8a94e14caf496aac33868a5189e
Dmitry Savin <the.great.demetrius@gmail.com>
2022-01-02 18:52:21 +0400

    Convert to Java style

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,14 +1,14 @@
 public class BubbleSort {
-    public static void main(String args[]) {
-        int arr[] = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
-        for(int i = arr.length - 1; i > 0; i--)
-            for(int j = 0; j < i; j++)
-                if(arr[j] > arr[j + 1]) {
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
+        for (int i = arr.length - 1; i > 0; i--)
+            for (int j = 0; j < i; j++)
+                if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = tmp;
                 }
-        for(int i : arr)
+        for (int i : arr)
             System.out.println(i);
     }
 }
