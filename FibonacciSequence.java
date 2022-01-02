186dc2145761df2308399af538200d85b7301ee9
Dmitry Savin <the.great.demetrius@gmail.com>
2022-01-02 19:04:06 +0400

    Convert to Java style

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -1,9 +1,9 @@
 public class FibonacciSequence {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.print(fib(8));
     }
+
     static int fib(int n) {
-        if(n < 2) return n;
-        return fib(n - 1) + fib(n - 2);
+        return n < 2 ? n : fib(n - 1) + fib(n - 2);
     }
 }
