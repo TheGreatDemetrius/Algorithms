26b841668dc64c24eac0ecae665f2fcca90ed335
Dmitry Savin <the.great.demetrius@gmail.com>
2022-01-02 18:55:45 +0400

    Convert to Java style

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,7 +1,7 @@
 public class CaesarCipher {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         String str = "Caesar cipher";
-        for(int i = 0; i < str.length(); i++)
+        for (int i = 0; i < str.length(); i++)
             System.out.print((char) (str.charAt(i) + 3));
     }
 }
