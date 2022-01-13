eded21dba379f933e83216751bacedcaace48b02
Dmitry Savin <the.great.demetrius@gmail.com>
2022-01-13 21:01:31 +0400

    Update and rename BubbleSort.java to sort/Bubble.java

--- sort/Bubble.java
+++ sort/Bubble.java
@@ -1,6 +1,6 @@
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
+        int[] arr = {1, 3, 5, 7, 9, 0, 2, 4, 6, 8};
         for (int i = arr.length - 1; i > 0; i--)
             for (int j = 0; j < i; j++)
                 if (arr[j] > arr[j + 1]) {
