a88f65a18d943493f4da1184563935e5e46c5f5f
Dmitry Savin <the.great.demetrius@gmail.com>
2022-03-16 04:47:01 +0400

    Fixed a bug with only closing brackets

--- BalancedBrackets.java
+++ BalancedBrackets.java
@@ -6,9 +6,9 @@ public class BalancedBrackets {
         ArrayDeque<Character> stack = new ArrayDeque<>();
         for (int i = 0; i < str.length(); i++)
             if (!stack.isEmpty() &&
-                    str.charAt(i) == ')' && stack.getFirst() == '(' ||
+                    (str.charAt(i) == ')' && stack.getFirst() == '(' ||
                     str.charAt(i) == ']' && stack.getFirst() == '[' ||
-                    str.charAt(i) == '}' && stack.getFirst() == '{')
+                    str.charAt(i) == '}' && stack.getFirst() == '{'))
                 stack.removeFirst();
             else
                 stack.addFirst(str.charAt(i));
