2afe7e020faa532323fff11a9f2a8469ec13b54d
Dmitry Savin <the.great.demetrius@gmail.com>
2022-03-16 04:55:02 +0400

    Update BalancedBrackets.java

--- BalancedBrackets.java
+++ BalancedBrackets.java
@@ -1,17 +1,11 @@
-import java.util.ArrayDeque;
-
 public class BalancedBrackets {
     public static void main(String[] args) {
-        String str = "([{}])";
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        for (int i = 0; i < str.length(); i++)
-            if (!stack.isEmpty() &&
-                    (str.charAt(i) == ')' && stack.getFirst() == '(' ||
-                    str.charAt(i) == ']' && stack.getFirst() == '[' ||
-                    str.charAt(i) == '}' && stack.getFirst() == '{'))
-                stack.removeFirst();
-            else
-                stack.addFirst(str.charAt(i));
-        System.out.print(stack.isEmpty());
+        System.out.println(isValid("([{}])"));
+    }
+
+    static boolean isValid(String str) {
+        int length = str.length();
+        str = str.replaceAll("\\(\\)|\\[]|\\{}", "");
+        return str.length() != length && (str.length() == 0 || isValid(str));
     }
 }
