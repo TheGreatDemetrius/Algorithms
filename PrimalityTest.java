0e42163b1e06d1f9219c84433b372aaea3759f19
Dmitry Savin <the.great.demetrius@gmail.com>
2022-10-09 00:12:33 +0400

    Convert to Java style

--- PrimalityTest.java
+++ PrimalityTest.java
@@ -4,11 +4,11 @@ public class PrimalityTest {
     }
 
     static boolean isPrime(int n) {
-	    if (n <= 1)
-		    return false;
-	    for (int i = 2; i < n; i++)
-		    if (n % i == 0)
-			    return false;
-	    return true;
+        if (n <= 1)
+            return false;
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+        return true;
     }
 }
